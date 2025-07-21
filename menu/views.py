220f39a1d383a86e6f51199138474393247b855d
Lolu38 <chiappetta.lucas38@gmail.com>
2025-07-21 18:21:26 +0200

    Starting the html

--- menu/views.py
+++ menu/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Type
 
 # Create your views here.
 def index(request):
-    return render(request, 'menu/index.html')
+    types = Type.objects.all()
+    return render(request, 'menu/index.html', {'types': types})
